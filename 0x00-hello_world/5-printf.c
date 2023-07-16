9f9993df7d78d63389530be58be72e571da72fce
Cor-vus1 <youssef.sam13579@gmail.com>
2023-07-15 19:05:55 -0700

    1

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
+#include <stdlib.h>
+
 /**
- * main - main block
- * Return: 0
+ * main - Entry point.
+ * description: Prints a sentence.
+ * Return: always 0
  */
-  
+
 int main(void)
 {
-  printf("with proper grammar, but the outcome is a     piece of art,\n");
-  return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,14 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
-
 /**
- * main - Entry point.
- * description: Prints a sentence.
- * Return: always 0
+ * main - Prints numbers from 0 to 9.
+ * Return: 0
  */
 
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
+	int x;
+
+	for (x = 0; x < 10; ++x)
+	{
+		printf("%d", x);
+	}
+	putchar('\n');
 	return (0);
 }
