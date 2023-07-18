b16faf7abb06a9d9a10a3ff64aee767b4205532b
Cor-vus1 <youssef.sam13579@gmail.com>
2023-07-18 15:03:00 -0700

    1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+* main - Entry point.
+* Description - prints the alphabet, in lowercase.
+* return:0
+*/
+
+int main(void)
+{
+	char x;
+
+	for (x = 'a'; x <= 'z'; x++)
+	{
+	_putchar(x);
+	_putchar('\n');
+	}
+
+	return (0);
+}
