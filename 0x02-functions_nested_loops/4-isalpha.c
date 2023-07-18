63a57950351122ae07a5791fdf8c91f86bcca93e
Cor-vus1 <youssef.sam13579@gmail.com>
2023-07-18 16:48:19 -0700

    1

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _isalpha - Shows 1 if input is a
+ * alpha charachter. Another cases, shows
+ * 0
+ *
+ * @c: The character in ASCII code
+ *
+ * Return: 1 for alpha charachters. 0 for the rest.
+*/
+
+int _isalpha(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
